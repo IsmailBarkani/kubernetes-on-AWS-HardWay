4f7bf6c243bf81496ffe9030a00b5f7aacbc2ea8
IsmailBarkani <ismail.barkani@um5s.net.ma>
2020-11-27 20:54:16 +0100

    configure node worker

--- node_shell.sh
+++ node_shell.sh
@@ -1,83 +1,65 @@
 #!/bin/bash
 
-exec &> /var/log/init-aws-kubernetes-node.log
+export master_ip=$1
+echo ${master_ip}
+export master_ip=$1
+echo ${master_ip}
+dnf -y upgrade
 
-set -o verbose
-set -o errexit
-set -o pipefail
+echo "disable SELinux"
+setenforce 0
+sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
 
-export KUBEADM_TOKEN=${kubeadm_token}
-export MASTER_IP=${master_private_ip}
-export KUBERNETES_VERSION="1.19.4"
+modprobe br_netfilter
 
-# Set this only after setting the defaults
-set -o nounset
+echo "Firewalld install"
+yum install firewalld -y
+systemctl start firewalld
+firewall-cmd --add-masquerade --permanent
+firewall-cmd --reload
 
-# We to match the hostname expected by kubeadm an the hostname used by kubelet
-FULL_HOSTNAME="$(curl -s http://169.254.169.254/latest/meta-data/hostname)"
+cat <<EOF > /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
 
+sysctl --system
 
+swapoff -a
 
-# Install docker
-yum install -y yum-utils device-mapper-persistent-data lvm2 docker
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+dnf -y install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
+dnf install docker-ce --nobest -y
+systemctl start docker
+systemctl enable docker
 
-# Install Kubernetes components
-sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+#Kubernetes installation
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
 enabled=1
 gpgcheck=1
 repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
-        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
 EOF
 
-# setenforce returns non zero if already SE Linux is already disabled
-is_enforced=$(getenforce)
-if [[ $is_enforced != "Disabled" ]]; then
-  setenforce 0
-  sed -i 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
-fi
+dnf upgrade -y
 
-yum install -y kubelet-$KUBERNETES_VERSION kubeadm-$KUBERNETES_VERSION kubernetes-cni
+dnf install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
 
-# Start services
-systemctl enable docker
-systemctl start docker
 systemctl enable kubelet
 systemctl start kubelet
 
-# Set settings needed by Docker
-sysctl net.bridge.bridge-nf-call-iptables=1
-sysctl net.bridge.bridge-nf-call-ip6tables=1
+firewall-cmd --zone=public --permanent --add-port={10250,30000-32767}/tcp
+firewall-cmd --reload
 
-# Fix certificates file on CentOS
-if cat /etc/*release | grep ^NAME= | grep CentOS ; then
-    rm -rf /etc/ssl/certs/ca-certificates.crt/
-    cp /etc/ssl/certs/ca-bundle.crt /etc/ssl/certs/ca-certificates.crt
-fi
+sleep 30s
 
-# Initialize the master
-cat >/tmp/kubeadm.yaml <<EOF
----
-apiVersion: kubeadm.k8s.io/v1
-kind: JoinConfiguration
-discovery:
-  bootstrapToken:
-    apiServerEndpoint: $MASTER_IP:6443
-    token: $KUBEADM_TOKEN
-    unsafeSkipCAVerification: true
-  timeout: 5m0s
-  tlsBootstrapToken: $KUBEADM_TOKEN
-nodeRegistration:
-  criSocket: /var/run/dockershim.sock
-  kubeletExtraArgs:
-    cloud-provider: aws
-    read-only-port: "10255"
-  name: $FULL_HOSTNAME
----
-EOF
+kubeadm join 10.0.1.88:6443 --token 4hznti.qpq36r5l1jggsm6t discovery-token-ca-cert-hash sha256:8463cd921ea311e3758fdbaac7e1228d346fe8045e76a8121538857e33a23659
 
-kubeadm reset --force
-kubeadm join --config /tmp/kubeadm.yaml
+
+
+
