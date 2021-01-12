c6081e38daef8c2c3b0a048ffacb6ddd798ccf73
IsmailBarkani <ismail.barkani@um5s.net.ma>
2021-01-12 02:47:12 +0100

    add liste

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-# test-STDT-INFRA
-=======
 # Projet-SDTD-Scripts-Terraform-Infra
->>>>>>> 111fd3f4700ec522de9a705801e9c61760b7b076
+
+:white_check_mark: ##fff
+:black_square_button:
+
--- node_shell.sh
+++ node_shell.sh
@@ -58,10 +58,6 @@ firewall-cmd --reload
 
 kubeadm join $master_ip:6443 --token $token --discovery-token-unsafe-skip-ca-verification --ignore-preflight-errors all
 
-
-#yum install -y httpd
-#systemctl start httpd
-#systemctl enable httpd
 sudo amazon-linux-extras install nginx1 -y
 sudo systemctl start nginx
 sudo firewall-cmd --zone=public --permanent --add-service=http
